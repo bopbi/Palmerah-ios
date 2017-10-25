43debb005ef804f900bbd49019878d20bd02e5c0
Bobby Prabowo <prabowo.bobby@gmail.com>
2017-10-25 07:10:10 +0700

    improve emoticon

--- Palmerah/String+Emoticon.swift
+++ Palmerah/String+Emoticon.swift
@@ -20,16 +20,10 @@ extension String {
             let prevChar = self[self.index(self.startIndex, offsetBy: index - 1)]
             let currentChar = self[self.index(self.startIndex, offsetBy: index)]
             if (prevChar == ":" && currentChar == "D") {
-                let attributedString = NSMutableAttributedString(string: self)
                 let emojiAttachment = NSTextAttachment()
                 emojiAttachment.image = emoticonImage
                 emojiAttachment.bounds = CGRect(x: 0, y: 0, width: emoticonImage.size.width, height: emoticonImage.size.height)
                 let emojiAttributedString = NSAttributedString(attachment: emojiAttachment)
-                let paragraphStyle = NSMutableParagraphStyle()
-                paragraphStyle.alignment = .natural
-                paragraphStyle.lineSpacing = 1.0
-                
-                attributedString.addAttribute(.paragraphStyle, value: paragraphStyle, range: NSMakeRange(0, attributedString.length))
                 stringWithEmoji.insert(emojiAttributedString, at: stringWithEmoji.length)
                 index += 1
             } else {
@@ -42,6 +36,10 @@ extension String {
             }
             index += 1
         }
+        let paragraphStyle = NSMutableParagraphStyle()
+        paragraphStyle.alignment = .natural
+        paragraphStyle.lineSpacing = 1.0
+        stringWithEmoji.addAttribute(.paragraphStyle, value: paragraphStyle, range: NSMakeRange(0, stringWithEmoji.length))
         return stringWithEmoji;
     }
  
